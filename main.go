4ca793c31406bd8cfaa986f7b569c2f7635eb5f4
Winson G <96694861+winsongr@users.noreply.github.com>
2022-03-06 13:33:26 +0530

    Update main.go

--- main.go
+++ main.go
@@ -16,19 +16,66 @@ func main() {
 	}
 
 	db.AutoMigrate(&user{})
-	User := user{
-		Name:  "wibi",
-		Lastn: "1608",
-		Cell:  1608,
-		Pin:   1608,
-	}
-	db.Create(&User)
+	// db.Migrator().DropTable(&user{})
+	// db.Migrator().CreateTable(&user{})
+	// User := user{
+	// 	Name:  "wibi",
+	// 	Lastn: "1608",
+	// 	Cell:  1608,
+	// 	Pin:   1608,
+	// }
+	// db.Create(&User)
+
+	// User := user{
+	// 	Id:    2,
+	// Name:  "wibi1608",
+	// Lastn: "wibi",
+	// Cell:  160868,
+	// Pin:   160816,
+	// }
+	// db.Updates(&User)
+	// User := user{
+	// 	Id: 4,
+	// }
+	// db.Delete(&User)
+	// 	User := []user{
+	// 		{Name: "wibi1608",
+	// 			Lastn: "wibi",
+	// 			Cell:  160868,
+	// 			Pin:   160816},
+	// 		{Name: "wibi1608",
+	// 			Lastn: "wibi",
+	// 			Cell:  160868,
+	// 			Pin:   160816},
+	// 		{Name: "wibi1608",
+	// 			Lastn: "wibi",
+	// 			Cell:  160868,
+	// 			Pin:   160816},
+	// 	}
+	// 	for _, User := range User {
+	// 		db.Create(&User)
+	// 	}
+	// User := user{
+	// 	Model: gorm.Model{
+	// 		CreatedAt: time.Now(),
+	// 	},
+	// }
+	// db.First(&User)
+	// db.Last(&User)
+	// db.Where("Cell", "1608").First(&User)
+	// fmt.Println(&User)
+User:=user{
+
+}
+db.Create(&User)
 }
 
 type user struct {
+	gorm.Model
 	Id    int
 	Name  string
 	Lastn string
-	Cell  int
-	Pin   int
+	Cell  int`gorm:"default:1"`
+	Pin   int `gorm:"size:100;null"`
+	Email string `gorm:"unique; not null"`
 }
